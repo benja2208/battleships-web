6e66cbe4af4fd7cb1d1be359900c3b402ff13ba4
raphlevy <raphlevy.perso@gmail.com>
2015-08-06 16:50:28 +0100

    Completed challenge with computer

--- features/starting_a_game.feature
+++ features/starting_a_game.feature
@@ -1,17 +1,17 @@
 Feature: Starting the game
-  As a person who wants to play battleships 
-  So that I know who I am 
+  As a person who wants to play battleships
+  So that I know who I am
   I want to be able to register for a game
 
   Scenario: Registering a user
     Given I am on the homepage
-    And I enter my details 
+    And I enter my details
     And I position my ships
     When I press "start"
     Then I should see "Hello, Stephen"
 
     Given I am on the homepage
     And I enter my details
-    And I position my ships 
+    And I position my ships
     When I press "start"
-    Then I should see "Waiting for player 2"
+    Then I should see "Go on then Stephen"
--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -7,24 +7,40 @@ require './lib/cell'
 require './lib/ship'
 
 class BattleshipsWeb < Sinatra::Base
-  $game = Game.new 
-  
+  $game = Game.new
+
   get '/' do
     erb :index
   end
 
-  post '/user' do 
+  post '/user' do
     $game.player_1 = Player.new(params[:name])
     board = Board.new({cell: Cell, size: 9, number_of_pieces: 1})
     board.fill_all_content(Water.new)
-    board.place(Ship.new(size:1), params[:ship_1_position].upcase.to_sym, params[:ship_1_orientation].to_sym)
-    @player_1 = $game.player_1.name
+    board.place(Ship.new(size: 1), params[:ship_1_position].upcase.to_sym, params[:ship_1_orientation].to_sym)
+    $game.player_1.board = board
+
+    $game.player_2 = Player.new("Computer")
+    $game.player_2.board = Board.new({cell: Cell, size: 9, number_of_pieces: 1})
+    $game.player_2.board.fill_all_content(Water.new)
+    $game.player_2.board.place(Ship.new(size: 1), params[:ship_1_position].upcase.to_sym, params[:ship_1_orientation].to_sym)
+      $game.player_2
+
     erb :welcome
-  end 
+  end
+
+  post 'shoot' do
+    $game.make_move params[:position].to_sym
+    if $game.over?
+      "Game is over"
+    else
+      "Bad luck"
+    end
+  end
 
 
   set :views, Proc.new { File.join(root, "..", "views") }
-  
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
